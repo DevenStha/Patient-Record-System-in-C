ddc9575b92dd3332e409a36245645088b008089c
DevenStha <devenshrestha777@gmail.com>
2024-05-13 09:29:32 +0545

    Added functions

--- demo_menu/main.c
+++ demo_menu/main.c
@@ -3,20 +3,8 @@
 #include <stdbool.h>
 #include <string.h>
 
-int main()
+void ask()
 {
-    system("color 17"); // 1 is blue 7 is white
-    char n;
-    /*printf("\t\t\t\t\t\t\tPatient Record System");
-    printf("\n\n\n\t\t\t\t----------------------------------------------------------");
-    printf("\n\t\t\t\t\t\t\t User Choice");
-    printf("\n\t\t\t\t----------------------------------------------------------");
-    printf("\n\t\t\t\t1. Add new patient");
-    printf("\n\t\t\t\t2. Search and modify patient's data");
-    printf("\n\t\t\t\t3. Exit\n ---> ");*/
-    while(true)
-    {
-    int choice;
     printf("\t\t\t\t\t\t\tPatient Record System");
     printf("\n\n\n\t\t\t\t----------------------------------------------------------");
     printf("\n\t\t\t\t\t\t\t User Choice");
@@ -24,18 +12,35 @@ int main()
     printf("\n\t\t\t\t1. Add new patient");
     printf("\n\t\t\t\t2. Search and modify patient's data");
     printf("\n\t\t\t\t3. Exit\n ---> ");
+    printf("Enter your choice here: ");
+}
+char *add()
+{
+    return "Add new patient\n";
+}
+char *update()
+{
+    return "Modity data\n";
+}
+int main()
+{
+    system("color 17"); // 1 is blue 7 is white
+    char n;
+    while(true)
+    {
+    int choice;
+    ask();
     scanf("%d", &choice);
     if(choice == 1)
     {
-        printf("Enter patient's name/address/sex/symptoms/etc...... Work in Progress!!!!!!\n");
+        printf(add());
     }
     else if(choice == 2)
     {
-        printf("Enter patient's code, update or move or view or remove ....... WIP!!!!!\n");
+        printf(update());
     }
     else if(choice == 3)
     {
-        printf("It will now exit");
         break;
     }
     else
