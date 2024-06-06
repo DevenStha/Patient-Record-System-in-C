2a954a93d7dd264b443a209a4a79817323f434a3
DevenStha <devenshrestha777@gmail.com>
2024-06-06 22:12:21 +0545

    added structs more functions lost my sanity and so on

--- demo_menu/main.c
+++ demo_menu/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdbool.h>
+#include <string.h>
 void ask()
 {
     printf("\t\t\t\t\t\t\tPatient Record System");
@@ -8,91 +9,121 @@ void ask()
     printf("\n\t\t\t\t----------------------------------------------------------");
     printf("\n\t\t\t\t1. Add new patient");
     printf("\n\t\t\t\t2. Update patient's data");
-    printf("\n\t\t\t\t3. Exit\n ---> ");
+    printf("\n\t\t\t\t3. Admin");
+    printf("\n\t\t\t\t4. Exit\n ---> ");
     printf("Enter your choice here: ");
 }
-void add()
+struct patient
 {
-    char f_name[99];
-<<<<<<< HEAD
-    char m_name[99] = " ";
-=======
-    char m_name[99] = {' '};
->>>>>>> c22d5c368703b8dd34ffb74a75f5b147eb8a9190
-    char l_name[99];
-    int year, month, day;
-    char symp[999];
-    char ad[50];
-    printf("Enter your first name: ");
-    scanf("%s", &f_name);
-    printf("\nEnter your middle name(Ctrl + C to skip): ");
-    scanf("%s", &m_name);
-    printf("\n\nEnter your last name: ");
-    scanf("%s", &l_name);
+    char name[99];
+    int year;
+    int month;
+    int day;
+    char address[99];
+    char symptoms[999];
+    long ph;
+    int code;
+
+}pt[];
+
+void add(int in)
+{
+    printf("\nEnter the full name of the Patient: ", in + 1);
+    scanf("%[^\t]s", &pt[in].name);
     printf("\nWhich year you were born in?(in numbers): ");
-    scanf("%d", &year);
+    scanf("%d", &pt[in].year);
     printf("\nWhich month you were born in?(in numbers): ");
-    scanf("%d", &month);
+    scanf("%d", &pt[in].month);
     printf("\nWhich day you were born in?(in numbers): ");
-    scanf("%d", &day);
-    printf("\nEnter your address: ");
-    scanf("%s", &ad);
-    printf("\nEnter your symptoms(seperated by dash): ");
-    scanf("%s", &symp);
-    printf("\n\nYour name is %s %s %s", f_name, m_name, l_name);
-    printf("\nDate of birth: %d-%d-%d", year,month,day);
-    printf("\nYou live in %s", ad);
-    printf("\nYou have following symptoms: %s\n\n", symp);
+    scanf("%d", &pt[in].day);
+    printf("Enter your address: ");
+    scanf("%[^\t]s", &pt[in].address);
+    printf("Enter your phone number: ");
+    scanf("%ld", &pt[in].ph);
+    printf("Enter your symptom: ");
+    scanf("%[^\t]s", &pt[in].symptoms);
 }
-void update()
+void print(int index)
 {
-<<<<<<< HEAD
-    printf("Modify Data\n");
-=======
-    return "Modify data\n";
->>>>>>> c22d5c368703b8dd34ffb74a75f5b147eb8a9190
+        pt[index].code = index + 1000;
+        printf("The patient with the following data has been added: \n\n");
+        printf("\nName: %s",pt[index].name);
+        printf("\nDate of birth: %d-%d-%d",pt[index].year,pt[index].month, pt[index].day);
+        printf("\nAddress: %s", pt[index].address);
+        printf("\nPhone number: %ld", pt[index].ph);
+        printf("\nSymptom: %s", pt[index].symptoms);
+        printf("\nYour code is %d", pt[index].code);
+}
+void modify()
+{
+    int in;
+    printf("Please enter the patient's code: ");
+    scanf("%d", &in);
+    in = 1000 - in;
+    printf("\nNew name: ", in + 1);
+    scanf("%[^\t]s", &pt[in].name);
+    printf("\nNew year(DOB): ");
+    scanf("%d", &pt[in].year);
+    printf("\nNew month(DOB): ");
+    scanf("%d", &pt[in].month);
+    printf("\nNew date(DOB): ");
+    scanf("%d", &pt[in].day);
+    printf("New address: ");
+    scanf("%[^\t]s", &pt[in].address);
+    printf("New phone number: ");
+    scanf("%ld", &pt[in].ph);
+    printf("Newer/updated symptom: ");
+    scanf("%[^\t]s", &pt[in].symptoms);
+    print(in);
 }
 int main()
 {
-    system("color 17"); // 1 is blue 7 is white
-    char a;
-    int num = 1;
-    while(num == 1)
+    int index = 0;
+    int c = 0;
+    int i = 0;
+    int confirm;
+    system("color 17");
+    while(true)
     {
-    int choice;
-    ask();
-    scanf("%d", &choice);
-    if(choice == 1)
-    {
-        add();
-    }
-    else if(choice == 2)
-    {
-        update();
-    }
-    else if(choice == 3)
-    {
-        break;
-    }
-    else
-    {
-        printf("Invalid input\n");
-    }
-    printf("Do you want to go back?(Y/N): ");
-<<<<<<< HEAD
-    scanf(" %c", &a);
-    if(a == 'N' || a == 'n')
-=======
-    scanf(" %c", &n);
-    if(n == 'N' || n == 'n')
->>>>>>> c22d5c368703b8dd34ffb74a75f5b147eb8a9190
-    {
-        break;
-    }
-    else if(a == 'Y' || a == 'y'){;}
-        else
+        ask();
+        char name[99];
+        scanf("%d",&i);
+        if(i == 2)
         {
-            printf("Invalid input");
+            break;
+        }
+        switch(i)
+        {
+        case 1:
+            add(index);
+            print(index);
+            ++index;
+            break;
+        case 2:
+            printf("How did you even get here?: ");
+            break;
+        case 3:
+            printf("WIP");
+            break;
+        case 4:
+            return 1;
+        default:
+            printf("Enter a valid choice: ");
+        }
+        printf("\n\nDo you want to add someone?\n\n1. Yes\n2. No: ");
+        scanf("%d", &confirm);
+        if(confirm != 1)
+        {
+            break;
+        }
+    }
+    while(i == 2)
+    {
+        modify();
+        printf("\n\nUpdate someone else?\n\n1. Yes\n2. No: ");
+        scanf("%d", &confirm);
+        if(confirm != 1)
+        {
             break;
         }
     }
